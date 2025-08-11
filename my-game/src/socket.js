91d29ce3dfcdcab627a7e0de9407081c67f67638
Ayush Mann <mannayush60@gmail.com>
2025-08-11 23:40:21 +0530

    deploying on gcp

--- my-game/src/socket.js
+++ my-game/src/socket.js
@@ -1,5 +1,5 @@
 
 import { io } from "socket.io-client";
 const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3000';
-const socket = socketIO(SERVER_URL);
+export const socket = socketIO(SERVER_URL);
 // export const socket = io("https://halo-havoc.onrender.com");
